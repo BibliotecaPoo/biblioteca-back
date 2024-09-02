cbaa565dbd3a07545cb6ee86c0a6d2377ac17403
Victor Vale <joaovictorvale06@gmail.com>
2024-09-02 16:45:27 -0300

    feat: migração das propriedade de administrador para alterar senha

---
+++ src/Biblioteca.Infra.Data/Migrations/20240902175247_Administrador_AdicionandoPropriedadeParaAlterarSenha.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Biblioteca.Infra.Data.Migrations
+{
+    public partial class Administrador_AdicionandoPropriedadeParaAlterarSenha : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CodigoDeRecuperacaoDeSenha",
+                table: "Administradores",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "PedidoDeRecuperacaoDeSenha",
+                table: "Administradores",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TempoDeExpiracaoDoCodigoDeRecuperacaoDeSenha",
+                table: "Administradores",
+                type: "datetime(6)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CodigoDeRecuperacaoDeSenha",
+                table: "Administradores");
+
+            migrationBuilder.DropColumn(
+                name: "PedidoDeRecuperacaoDeSenha",
+                table: "Administradores");
+
+            migrationBuilder.DropColumn(
+                name: "TempoDeExpiracaoDoCodigoDeRecuperacaoDeSenha",
+                table: "Administradores");
+        }
+    }
+}
