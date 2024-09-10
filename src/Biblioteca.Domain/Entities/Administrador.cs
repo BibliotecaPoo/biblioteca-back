ef6465ceb366be5c3f0e8f17584c6eada8601df7
Victor Vale <joaovictorvale06@gmail.com>
2024-09-10 16:18:02 -0300

    feat: atribuindo novas propriedades de recuperação de senha para administrador e gerando migração no banco de dados

--- src/Biblioteca.Domain/Entities/Administrador.cs
+++ src/Biblioteca.Domain/Entities/Administrador.cs
@@ -2,4 +2,7 @@ namespace Biblioteca.Domain.Entities;
 
 public class Administrador : Pessoa
 {
+    public string? CodigoDeRecuperacaoDeSenha { get; set; }
+    public DateTime? TempoDeExpiracaoDoCodigoDeRecuperacaoDeSenha { get; set; }
+    public bool PedidoDeRecuperacaoDeSenha { get; set; }
 }
